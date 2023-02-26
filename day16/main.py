941b502ec9899e2a3262aa8719d13fba2307520d
Artur <a.mokrzycki@gmail.com>
2023-02-26 23:03:52 +0000

    opp_coffee_2

--- day16/main.py
+++ day16/main.py
@@ -1,3 +1,15 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_menu =  Menu()
+coffee_machine = CoffeeMaker()
+
+order = input(f"What would you like? {coffee_menu.get_items()}: ")
+drink = coffee_menu.find_drink(order)
+#coffee_machine.is_resource_sufficient(drink)
+
+
+#check_resources = coffee_machine.is_resource_sufficient(drink)
+
+#print(f"{drink}")
