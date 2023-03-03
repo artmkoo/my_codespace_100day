73c8a34d68e09d1a1f44cc569deb0f248babe358
Artur <a.mokrzycki@gmail.com>
2023-03-03 15:51:14 +0000

    day17-20230303

--- day16/main.py
+++ day16/main.py
@@ -2,21 +2,23 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-coffee_menu =  Menu()
+menu =  Menu()
 coffee_machine = CoffeeMaker()
 money_machine = MoneyMachine()
 
 is_working = True
 
 while is_working:
-    order = input(f"What would you like? {coffee_menu.get_items()}: ")
+    order = input(f"What would you like? {menu.get_items()}: ")
     if order == "turnoff":
         is_working = False
-    drink = coffee_menu.find_drink(order)
-    if drink and coffee_machine.is_resource_sufficient(drink):
-        #coffee_machine.report()
-        #money_machine.report()
-        if money_machine.make_payment(drink.cost):
-            coffee_machine.make_coffee(drink)
-            coffee_machine.report()
-            money_machine.report()
+    elif order == "report":
+        coffee_machine.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(order)
+        if drink and coffee_machine.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_machine.make_coffee(drink)
+                coffee_machine.report()
+                money_machine.report()
